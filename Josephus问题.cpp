59b90f2214e79d1fb351f9f2272ac41b23d93f2b
XinliJing <xinlijing@outlook.com>
2016-11-26 21:01:11 +0800

    约瑟夫问题~
    QAQ我好腻害

---
+++ Josephus问题.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+	int s, m, n, k, as, j = 0, t;
+	printf("output the number of prisoners:");
+	scanf("%d", &n);
+	printf("output the begin but not die:");
+	scanf("%d", &s);
+	printf("input what number will be counted:");
+	scanf("%d", &m);
+	int a[n] = {0};
+	s = s-1;
+	as = m;
+	while(j < n-1){
+		m = as;
+		for(int i = 1; i <= m; i++){
+			k = (s+i)%n;
+			if(a[k] == 1){
+				m++;
+				j++;
+			}
+		}
+		a[k] = 1;
+		s = k;
+	}
+	for(int i = 0; i < n; i++){
+		if(a[i] == 0)
+			t = i;
+	}
+	printf("%d", t+1);
+	 
+//	for(int i = 0; i < n; i++)
+//		printf("%d ", a[i]);
+	return 0;
+}
